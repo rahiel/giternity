44570f9de8d58df5e28ee4d29dcd73f77ca82d24
Rahiel Kasim <rahielkasim@gmail.com>
2019-03-25 21:01:48 +0100

    release 0.5

--- giternity.py
+++ giternity.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # giternity - Mirror git repositories and retrieve metadata for cgit
-# Copyright (C) 2017-2018 Rahiel Kasim
+# Copyright (C) 2017-2019 Rahiel Kasim
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -28,7 +28,7 @@ import requests
 import toml
 
 
-__version__ = "0.4"
+__version__ = "0.5"
 
 
 def main():
